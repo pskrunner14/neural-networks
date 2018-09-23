0b64e2c0f45ed36efb77402fcc3690fee1397311
Prabhsimran Singh <pskrunner14@gmail.com>
2018-09-23 17:37:34 +0530

    ref docstring

--- layers.py
+++ layers.py
@@ -5,6 +5,10 @@ from gpu import cuda_mat_mul, cuda_mat_sum
 
 np.random.seed(42)
 
+"""
+GPU Computation Methods
+"""
+
 def compute_dot_prod(a, b):
     m, n, k = a.shape[0], a.shape[1], b.shape[1]
     a = a.flatten()
@@ -21,45 +25,17 @@ def compute_elem_wise_sum(a, b):
     cuda_mat_sum(a.astype(np.float32), b.astype(np.float32), c, m, n)
     return c.reshape((m, n))
 
-class Layer:
-    
-    """ A building block. Each layer is capable of performing two things:
-        - Process input to get output:           output = layer.forward(input)
-        - Propagate gradients through itself:    grad_input = layer.backward(input, grad_output)
-        Some layers also have learnable parameters which they update during layer.backward.
-    """
+""" Dense Layer
 
-    def __init__(self):
-        """Here you can initialize layer parameters (if any) and auxiliary stuff."""
-        pass
-    
-    def forward(self, A):
-        """ Takes input data of shape [batch, input_units], 
-            returns output data [batch, output_units]
-        """
-        # A dummy layer just returns whatever it gets as input.
-        return A
+Performs a learned affine transformation:
+    f(x) = <W*x> + b
 
-    def backward(self, A_prev, dZ, **kwargs):
-        """ Performs a backpropagation step through the layer, with respect to the given input.
-            To compute loss gradients w.r.t input, you need to apply chain rule (backprop):
-            d loss / d x  = (d loss / d layer) * (d layer / d x)
-            Luckily, you already receive d loss / d layer as input, 
-            so you only need to multiply it by d layer / d x.
-            If your layer has parameters (e.g. dense layer), 
-            you also need to update them here using d loss / d layer
-        """
-        # The gradient of a dummy layer is precisely grad_output, but we'll write it more explicitly
-        num_units = A_prev.shape[1]
-        d_layer_d_input = np.eye(num_units)
-        return np.dot(dZ, d_layer_d_input) # chain rule
-
-class Dense(Layer):
+    input shape: [batch, input_units]
+    output shape: [batch, output units]
+"""
+class Dense():
     
     def __init__(self, input_units, output_units):
-        """ A dense layer is a layer which performs a learned affine transformation:
-            f(x) = <W*x> + b
-        """
         # initialize weights with small random numbers. We use xavier initialization
         self.weights = np.random.randn(input_units, output_units) * np.sqrt(2. / (input_units + output_units))
         self.biases = np.zeros(output_units)
@@ -67,12 +43,6 @@ class Dense(Layer):
         self.g2_biases = np.zeros_like(self.biases)
         
     def forward(self, A):
-        """ Perform an affine transformation:
-            f(x) = <W*x> + b
-            
-            input shape: [batch, input_units]
-            output shape: [batch, output units]
-        """
         Wx = compute_dot_prod(A, self.weights)
         bias = self.biases
         bias = np.repeat(bias, Wx.shape[0], axis=0)
@@ -102,7 +72,6 @@ class Dense(Layer):
         if optim == 'rmsprop':
             self.g2_weights = (alpha * self.g2_weights) + (1 - alpha) * np.square(grad_weights)
             self.g2_biases = (alpha * self.g2_biases) + (1 - alpha) * np.square(grad_biases)
-            
             # Here we perform a stochastic gradient descent step. 
             # Later on, you can try replacing that with something better.
             self.weights -= update_weights / np.sqrt(self.g2_weights + epsilon)
@@ -113,16 +82,22 @@ class Dense(Layer):
 
         return grad_input
     
-class ReLU(Layer):
+"""ReLU layer 
+
+Applies elementwise rectified linear unit to all inputs:
+    f(x) = max(0, x)
+
+    input shape: [batch, input_units]
+    output shape: [batch, input_units]
+"""
+class ReLU():
+
     def __init__(self):
-        """ReLU layer simply applies elementwise rectified linear unit to all inputs"""
         self.type = 'relu'
     
     def forward(self, A):
-        """Apply elementwise ReLU to [batch, input_units] matrix"""
         return np.maximum(0, A)
     
     def backward(self, A_prev, dZ, **kwargs):
-        """Compute gradient of loss w.r.t. ReLU input"""
         relu_grad = A_prev > 0
         return dZ * relu_grad
